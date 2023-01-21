76871cf0072aa76e2e56fa6a80b52ae106050186
jayflan <j.c.flanary@gmail.com>
2023-01-21 08:38:43 -0500

    deploy testing

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
+require('dotenv').config;
 
 const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
@@ -12,13 +13,13 @@ if (process.env.LOGGING === "true") {
   delete config.logging;
 }
 
-if (process.env.DATABASE_URL) {
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-}
+// if (process.env.DATABASE_URL) {
+//   config.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   };
+// }
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost/cap_t3`,
