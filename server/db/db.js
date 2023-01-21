49fc6e9a3139c2b27ecf579015b4c5ea85940e61
jayflan <j.c.flanary@gmail.com>
2023-01-21 09:35:39 -0500

    deploy testing

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
-// require('dotenv').config;
+require('dotenv').config();
 
 const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
@@ -21,6 +21,7 @@ if (process.env.LOGGING === "true") {
 //   };
 // }
 
+console.log(process.env.DATABASE_URL)
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost/${databaseName}`,
   config
